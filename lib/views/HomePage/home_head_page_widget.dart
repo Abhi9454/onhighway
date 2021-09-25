6849379f03aeaf5c01eda3c7e752d8470384e5dd
abhi94548 <abhishek.m@accubits.com>
2021-09-25 21:46:21 +0530

    Bottom Navigation Bar Added and changes in request service page

--- lib/views/HomePage/home_head_page_widget.dart
+++ lib/views/HomePage/home_head_page_widget.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import '../HomePage/widget/home_text_widget.dart';
 import '../HomePage/home_body_page_widget.dart';
-import '../../config.dart';
-import '../../helpers/clipper_clip_path.dart';
 
 class HomePageHeadWidget extends StatelessWidget {
   const HomePageHeadWidget({Key? key}) : super(key: key);
